ebd69f6a6ed2bf1ac7731e89d1d200b89efd2275
xDarkixx <andygeiffes35@gmail.com>
2026-09-03 17:17:41 +0100

    Port computer block to NeoForge 26.2 codec API

--- src/main/java/de/xdarkixx/minecraft/opencomputers/ComputerBlock.java
+++ src/main/java/de/xdarkixx/minecraft/opencomputers/ComputerBlock.java
@@ -1,5 +1,6 @@
 package de.xdarkixx.minecraft.opencomputers;
 
+import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
@@ -10,8 +11,15 @@ import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
 public final class ComputerBlock extends BaseEntityBlock {
+    public static final MapCodec<ComputerBlock> CODEC = simpleCodec(ComputerBlock::new);
+
     public ComputerBlock(Properties properties) {
         super(properties);
+    }
+
+    @Override
+    protected MapCodec<? extends BaseEntityBlock> codec() {
+        return CODEC;
     }
 
     @Override
