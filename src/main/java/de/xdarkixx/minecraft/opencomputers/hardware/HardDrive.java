42290c7dc27c2e914a899d8d4a950981f8593b3e
xDarkixx <andygeiffes35@gmail.com>
2026-09-03 18:41:31 +0100

    Add virtual persistent hard-drive model

---
+++ src/main/java/de/xdarkixx/minecraft/opencomputers/hardware/HardDrive.java
@@ -0,0 +1,23 @@
+package de.xdarkixx.minecraft.opencomputers.hardware;
+
+/** Persistent virtual hard-drive model. Storage never escapes into the host filesystem. */
+public final class HardDrive {
+    private final int capacityBytes;
+    private final VirtualFileSystem filesystem;
+
+    public HardDrive(int capacityBytes) {
+        if (capacityBytes < 4 * 1024 || capacityBytes > 64 * 1024 * 1024) {
+            throw new IllegalArgumentException("hard-drive capacity out of bounds");
+        }
+        this.capacityBytes = capacityBytes;
+        this.filesystem = new VirtualFileSystem();
+    }
+
+    public int capacityBytes() {
+        return capacityBytes;
+    }
+
+    public VirtualFileSystem filesystem() {
+        return filesystem;
+    }
+}
