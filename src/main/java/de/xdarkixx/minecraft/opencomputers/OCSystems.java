f01b5db31cc39cf9b04ea38985ac685978ca8ba2
xDarkixx <andygeiffes35@gmail.com>
2026-09-03 21:46:40 +0100

    Fix OCSystems Map import

--- src/main/java/de/xdarkixx/minecraft/opencomputers/OCSystems.java
+++ src/main/java/de/xdarkixx/minecraft/opencomputers/OCSystems.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
