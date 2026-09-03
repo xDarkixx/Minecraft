41d56c0d8c103c9df97235e0ff226ce115068baa
xDarkixx <andygeiffes35@gmail.com>
2026-09-03 06:20:03 +0100

    Add shared community build information

---
+++ src/main/java/de/xdarkixx/minecraftcommunity/BuildInfo.java
@@ -0,0 +1,11 @@
+package de.xdarkixx.minecraftcommunity;
+
+/** Build constants for the Minecraft Community project. */
+public final class BuildInfo {
+    public static final String VERSION = "0.1.0";
+    public static final String MINECRAFT_VERSION = "26.2";
+    public static final String NEOFORGE_VERSION = "26.2.0.62";
+
+    private BuildInfo() {
+    }
+}
